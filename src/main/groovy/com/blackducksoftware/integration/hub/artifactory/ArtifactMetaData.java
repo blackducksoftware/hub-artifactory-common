d01c885b8bca8a121c95271f3c72ac41c0880928
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-12-22 11:25:59 -0500

    using components directly

--- src/main/groovy/com/blackducksoftware/integration/hub/artifactory/ArtifactMetaData.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/artifactory/ArtifactMetaData.java
@@ -1,10 +1,32 @@
 package com.blackducksoftware.integration.hub.artifactory;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.RecursiveToStringStyle;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
 public class ArtifactMetaData {
+    public String componentVersionLink;
     public int highSeverityCount;
     public int mediumSeverityCount;
     public int lowSeverityCount;
     public String policyStatus;
     public String forge;
     public String originId;
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this, RecursiveToStringStyle.JSON_STYLE);
+    }
+
 }
--- src/main/groovy/com/blackducksoftware/integration/hub/artifactory/ArtifactMetaDataManager.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/artifactory/ArtifactMetaDataManager.java
@@ -13,7 +13,7 @@ import com.blackducksoftware.integration.exception.IntegrationException;
 import com.blackducksoftware.integration.hub.api.view.MetaHandler;
 import com.blackducksoftware.integration.hub.model.view.ComponentVersionView;
 import com.blackducksoftware.integration.hub.model.view.ProjectVersionView;
-import com.blackducksoftware.integration.hub.model.view.VulnerableComponentView;
+import com.blackducksoftware.integration.hub.model.view.VulnerabilityView;
 import com.blackducksoftware.integration.hub.service.HubService;
 
 public class ArtifactMetaDataManager {
@@ -21,29 +21,6 @@ public class ArtifactMetaDataManager {
 
     public List<ArtifactMetaData> getMetaData(final HubService hubService, final ProjectVersionView projectVersionView) throws IntegrationException {
         final Map<String, ArtifactMetaData> idToArtifactMetaData = new HashMap<>();
-
-        final List<VulnerableComponentView> vulnerableComponentViews = hubService.getAllViewsFromLink(projectVersionView, MetaHandler.VULNERABLE_COMPONENTS_LINK, VulnerableComponentView.class);
-        for (final VulnerableComponentView vulnerableComponentView : vulnerableComponentViews) {
-            final String forge = vulnerableComponentView.componentVersionOriginName;
-            final String originId = vulnerableComponentView.componentVersionOriginId;
-            if (StringUtils.isNotBlank(forge) && StringUtils.isNotBlank(originId)) {
-                final String severity = vulnerableComponentView.vulnerabilityWithRemediation.severity;
-                if (!idToArtifactMetaData.containsKey(key(forge, originId))) {
-                    final ArtifactMetaData artifactMetaData = new ArtifactMetaData();
-                    artifactMetaData.forge = forge;
-                    artifactMetaData.originId = originId;
-                    idToArtifactMetaData.put(key(forge, originId), artifactMetaData);
-                }
-
-                if ("LOW".equals(severity)) {
-                    idToArtifactMetaData.get(key(forge, originId)).lowSeverityCount++;
-                } else if ("MEDIUM".equals(severity)) {
-                    idToArtifactMetaData.get(key(forge, originId)).mediumSeverityCount++;
-                } else if ("HIGH".equals(severity)) {
-                    idToArtifactMetaData.get(key(forge, originId)).highSeverityCount++;
-                }
-            }
-        }
 
         final List<VersionBomComponentRevisedView> versionBomComponents = hubService.getAllViewsFromLink(projectVersionView, MetaHandler.COMPONENTS_LINK, VersionBomComponentRevisedView.class);
         versionBomComponents.forEach(versionBomComponent -> {
@@ -54,16 +31,14 @@ public class ArtifactMetaDataManager {
                 originViews.forEach(originView -> {
                     final String forge = originView.originName;
                     final String originId = originView.originId;
-                    if (StringUtils.isNotBlank(forge) && StringUtils.isNotBlank(originId)) {
-                        final String policyStatus = versionBomComponent.policyStatus;
-                        if (!idToArtifactMetaData.containsKey(key(forge, originId))) {
-                            final ArtifactMetaData artifactMetaData = new ArtifactMetaData();
-                            artifactMetaData.forge = forge;
-                            artifactMetaData.originId = originId;
-                            idToArtifactMetaData.put(key(forge, originId), artifactMetaData);
-                        }
+                    if (StringUtils.isNotBlank(forge) && StringUtils.isNotBlank(originId) && !idToArtifactMetaData.containsKey(key(forge, originId))) {
+                        final ArtifactMetaData artifactMetaData = new ArtifactMetaData();
+                        artifactMetaData.forge = forge;
+                        artifactMetaData.originId = originId;
+                        artifactMetaData.policyStatus = versionBomComponent.policyStatus;
+                        populateVulnerabilityCounts(artifactMetaData, versionBomComponent, hubService);
 
-                        idToArtifactMetaData.get(key(forge, originId)).policyStatus = policyStatus;
+                        idToArtifactMetaData.put(key(forge, originId), artifactMetaData);
                     }
                 });
             } catch (final IntegrationException e) {
@@ -72,6 +47,24 @@ public class ArtifactMetaDataManager {
         });
 
         return new ArrayList<>(idToArtifactMetaData.values());
+    }
+
+    private void populateVulnerabilityCounts(final ArtifactMetaData artifactMetaData, final VersionBomComponentRevisedView versionBomComponent, final HubService hubService) {
+        final String vulnerabilitiesLink = hubService.getFirstLinkSafely(versionBomComponent, MetaHandler.VULNERABILITIES_LINK);
+        try {
+            final List<VulnerabilityView> componentVulnerabilities = hubService.getAllViews(vulnerabilitiesLink, VulnerabilityView.class);
+            componentVulnerabilities.forEach(vulnerability -> {
+                if ("HIGH".equals(vulnerability.severity)) {
+                    artifactMetaData.highSeverityCount++;
+                } else if ("MEDIUM".equals(vulnerability.severity)) {
+                    artifactMetaData.mediumSeverityCount++;
+                } else if ("LOW".equals(vulnerability.severity)) {
+                    artifactMetaData.lowSeverityCount++;
+                }
+            });
+        } catch (final IntegrationException e) {
+            logger.error(String.format("Can't populate vulnerability counts for %s: %s", vulnerabilitiesLink, e.getMessage()));
+        }
     }
 
     private String key(final String forge, final String originId) {
