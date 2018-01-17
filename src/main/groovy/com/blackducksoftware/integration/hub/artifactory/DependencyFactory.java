eafd10808e6a82cee8503d137c7fb559bfd104e5
Richard Otte <rotte@blackducksoftware.com>
2018-01-17 12:35:09 -0500

    Adding copyright header to DependencyFactory, fixing bug

--- src/main/groovy/com/blackducksoftware/integration/hub/artifactory/DependencyFactory.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/artifactory/DependencyFactory.java
@@ -1,3 +1,26 @@
+/**
+ * hub-artifactory-common
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.blackducksoftware.integration.hub.artifactory;
 
 import org.apache.commons.lang3.StringUtils;
@@ -59,11 +82,9 @@ public class DependencyFactory {
     }
 
     private Dependency createNameVersionDependencyFromProperties(final Forge forge, final org.artifactory.md.Properties properties, final String namePropertyName, final String versionPropertyName) {
-        String name;
-        String version;
-        if (null != properties.get(namePropertyName) && null != properties.get(versionPropertyName)) {
-            name = properties.get(namePropertyName).iterator().next();
-            version = properties.get(versionPropertyName).iterator().next();
+        final String name = properties.getFirst(namePropertyName);
+        final String version = properties.getFirst(versionPropertyName);
+        if (StringUtils.isNotEmpty(name) && StringUtils.isNotEmpty(version)) {
             return createNameVersionDependency(forge, name, version);
         }
         return null;
