3fc69bb8ee6ffa57320f98c64b4b39c25d62cdb6
Richard Otte <rotte@blackducksoftware.com>
2018-03-26 14:21:39 -0400

    fixing test

--- src/test/groovy/com/blackducksoftware/integration/hub/artifactory/HubModelTransformerTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/artifactory/HubModelTransformerTest.groovy
@@ -15,13 +15,13 @@ class HubModelTransformerTest {
         def projectVersionView = new ProjectVersionView();
         def projectVersionViewMeta = new ResourceMetadata();
         projectVersionViewMeta.href = projectVersionLink;
-        projectVersionView.meta = projectVersionViewMeta;
+        projectVersionView._meta = projectVersionViewMeta;
 
         String componentVersionLink = 'https://int-hub04.dc1.lan/api/components/dc3dee66-4939-4dea-b22f-ead288b4f117/versions/f9e2e6ff-7340-4fb3-a29f-a6fa98a10bfe'
         def componentVersionView = new ComponentVersionView();
         def componentVersionViewMeta = new ResourceMetadata();
         componentVersionViewMeta.href = componentVersionLink;
-        componentVersionView.meta = componentVersionViewMeta;
+        componentVersionView._meta = componentVersionViewMeta;
 
         String expectedProjectVersionComponentLink = projectVersionLink + '/components/dc3dee66-4939-4dea-b22f-ead288b4f117/versions/f9e2e6ff-7340-4fb3-a29f-a6fa98a10bfe'
         Assert.assertEquals(expectedProjectVersionComponentLink, hubModelTransformer.getProjectVersionComponentLink(projectVersionView, componentVersionView))
